b4e802dbd73721672d09ad22e8f0107952f9f97e
Zachary Donato <zdonato@stevens.edu>
2014-11-19 19:11:05 -0500

    Page for handling success/failure of form.

---
+++ submit.php
@@ -0,0 +1,26 @@
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/contact.css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="javascript/smoothscroll.js"></script>
+        <title>Stevens Computer Science Club</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><h1><a href="index.html">SC<span class="superscript">2</span></a></h1></li>
+                    <li><a href="index.html#about">About Us</a></li>
+                    <li><a href="index.html#events">Events</a></li>
+                    <li><a href="index.html#projects">Projects</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="index.html#">Anchor #5</a></li>
+                </ul>
+            </nav>
+        </header>
+        <footer>
+            Some social media stuff here, orgsync, idfk
+        </footer>
+    </body>
+</html>
